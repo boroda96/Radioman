880a947c6ecc7a6fae9f6c3b3cd814e6115575e8
Kurtsev Dmitriy <boman@bk.ru>
2023-02-19 15:18:13 +0500

    add Tests

--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -6,21 +6,39 @@ import org.junit.jupiter.api.Test;
 public class RadioTest {
 
     @Test
-    public void shouldSetRadioAndVolume() {
+    public void shouldSetRadio10() {
+        Radio radio = new Radio();
+
+        radio.setNumberRadio(10);
+
+        int expected = 0;
+        int actual = radio.numberRadio;
+
+        Assertions.assertEquals(expected, actual);
+        }
+    @Test
+    public void shouldSetRadio5() {
         Radio radio = new Radio();
 
         radio.setNumberRadio(5);
-        radio.setVolume(6);
 
-        int expectedN = 5;
-        int expectedV = 6;
+        int expected = 5;
+        int actual = radio.numberRadio;
 
-        int actualN = radio.numberRadio;
-        int actualV = radio.volume;
-
-        Assertions.assertEquals(expectedN, actualN);
-        Assertions.assertEquals(expectedV, actualV);
+        Assertions.assertEquals(expected, actual);
     }
+    @Test
+    public void shouldSetRadio0() {
+        Radio radio = new Radio();
+
+        radio.setNumberRadio(-1);
+
+        int expected = 0;
+        int actual = radio.numberRadio;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 
     @Test
     public void shouldNextNumberRadio() {
@@ -29,6 +47,28 @@ public class RadioTest {
         radio.nextNumberRadio();
 
         int exp = 0;
+        int act = radio.getNumberRadio();
+
+        Assertions.assertEquals(exp, act);
+    }
+    @Test
+    public void shouldNextNumberRadio1() {
+        Radio radio = new Radio();
+        radio.setNumberRadio(0);
+        radio.nextNumberRadio();
+
+        int exp = 1;
+        int act = radio.getNumberRadio();
+
+        Assertions.assertEquals(exp, act);
+    }
+    @Test
+    public void shouldNextNumberRadio2() {
+        Radio radio = new Radio();
+        radio.setNumberRadio(1);
+        radio.nextNumberRadio();
+
+        int exp = 2;
         int act = radio.getNumberRadio();
 
         Assertions.assertEquals(exp, act);
@@ -45,6 +85,17 @@ public class RadioTest {
 
         Assertions.assertEquals(exp, act);
     }
+    @Test
+    public void shouldPrevNumberRadio8() {
+        Radio radio = new Radio();
+        radio.setNumberRadio(9);
+        radio.prevNumberRadio();
+
+        int exp = 8;
+        int act = radio.getNumberRadio();
+
+        Assertions.assertEquals(exp, act);
+    }
 
     @Test
     public void shouldNextVolume() {
@@ -57,14 +108,58 @@ public class RadioTest {
 
         Assertions.assertEquals(exp, act);
     }
+    @Test
+    public void shouldNextVolume1() {
+        Radio radio = new Radio();
+        radio.setVolume(0);
+        radio.nextVolume();
+
+        int exp = 1;
+        int act = radio.getVolume();
+
+        Assertions.assertEquals(exp, act);
+    }
+    @Test
+    public void shouldNextVolume10() {
+        Radio radio = new Radio();
+        radio.setVolume(11);
+        radio.nextVolume();
+
+        int exp = 10;
+        int act = radio.getVolume();
+
+        Assertions.assertEquals(exp, act);
+    }
 
     @Test
     public void shouldPrevVolume() {
         Radio radio = new Radio();
-        radio.setVolume(9);
+        radio.setVolume(11);
         radio.prevVolume();
 
-        int exp = 8;
+        int exp = 9;
+        int act = radio.getVolume();
+
+        Assertions.assertEquals(exp, act);
+    }
+    @Test
+    public void shouldPrevVolume0() {
+        Radio radio = new Radio();
+        radio.setVolume(1);
+        radio.prevVolume();
+
+        int exp = 0;
+        int act = radio.getVolume();
+
+        Assertions.assertEquals(exp, act);
+    }
+    @Test
+    public void shouldPrevVolume00() {
+        Radio radio = new Radio();
+        radio.setVolume(0);
+        radio.prevVolume();
+
+        int exp = 0;
         int act = radio.getVolume();
 
         Assertions.assertEquals(exp, act);
